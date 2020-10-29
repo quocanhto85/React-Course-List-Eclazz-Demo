7021b0fbe045d717e545ad4b82c479f6ab5989c4
anhtpq <quocanhto85@gmail.com>
2020-10-29 15:51:01 +0700

    create Login UI, handle authentication API, config router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Login from "./screens/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    BrowserRouter as Router,
+    Route,
+} from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/main" component={() => {
+                    window.location.href = "https://eclass.ftech.ai/"; 
+                    return null;
+                }} />
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
